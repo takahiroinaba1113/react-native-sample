052a15b265c0e91eb8d8a76d5a6e82c8629e2797
takahiroinaba <takahiroinaba1113@gmail.com>
2021-06-10 22:42:48 +0900

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  quoteProps: as-needed,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  endOfLine: lf,
+  requirePragma: false,
+  insertPragma: false,
+};
